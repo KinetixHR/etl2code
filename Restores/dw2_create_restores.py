da127b08e5352cc0e12cf61bb5ff85476f826396
azureuser <azureuser@azureVM>
2024-03-18 21:14:06 +0000

    changing naming scheme

---
+++ Restores/dw2_create_restores.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import sqlalchemy
+import urllib
+
+today = pd.to_datetime("today")
+
+#Azure SQL Database credentials
+SERVER = "kinetixsql.database.windows.net" 
+DATABASE = "KinetixSQL" 
+USERNAME = "awhelan" 
+PASSWORD = "5uj7*ZpE8Y$D"
+DRIVER="ODBC Driver 18 for SQL Server"
+constring = "mssql+pyodbc:///?odbc_connect={}".format(urllib.parse.quote_plus("DRIVER=ODBC Driver 18 for SQL Server;SERVER={0};PORT=1433;DATABASE={1};UID={2};PWD={3};TDS_Version=8.0;".format(SERVER, DATABASE, USERNAME, PASSWORD)))
+engine = sqlalchemy.create_engine(constring,echo=False)
+
+print("Starting JOBS TABLE backup")
+df = pd.read_sql("SELECT * FROM dw2_jobs",con = engine)
+df.to_csv(f"jobs_table_extract for {today}.csv")
+print("JOBS TABLE extract created successfully")
+
+print("Starting USERS TABLE backup")
+df = pd.read_sql("SELECT * FROM dw2_users",con = engine)
+df.to_csv(f"users_table_extract for {today}.csv")
+print("USERS TABLE extract created successfully")
+
+print("Starting PLACEMENTS TABLE backup")
+df = pd.read_sql("SELECT * FROM dw2_placements",con = engine)
+df.to_csv(f"placements_table_extract for {today}.csv")
+print("PLACEMENTS TABLE extract created successfully")
